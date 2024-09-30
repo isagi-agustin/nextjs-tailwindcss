b167d792ca0bd38bb6a7d2f556764b68ae963032
isagi-agustin <isagiagustin@outlook.com>
2024-09-30 10:59:58 -0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import PRODUCTS from "./products";
+import { ProductCard } from "./ProductCard";
+
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      {PRODUCTS.map((product) => (
+        <div key={product.id} className={styles.card}>
+          <ProductCard product={product} />
+        </div>
+      ))}
+    </main>
+  );
+}
