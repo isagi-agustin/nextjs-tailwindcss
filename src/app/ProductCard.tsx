0380f1cb0723e17665f127a69ed1413f8ddd6ef7
isagi-agustin <isagiagustin@outlook.com>
2024-10-01 08:47:31 -0700

    combine css module with tailwind css

--- src/app/ProductCard.tsx
+++ src/app/ProductCard.tsx
@@ -10,15 +10,15 @@ interface Props {
 
 export const ProductCard = ({ product }: Props) => {
   return (
-    <div className="@container">
-      <div className="flex flex-col @md:flex-row">
-        <div className="w-full @md:w-1/4">
+    <div className={styles.card}>
+      <div className={styles.cardContainer}>
+        <div className={styles.imageContainer}>
           <Image
             src={product.image}
             alt={product.title}
             width={300}
             height={300}
-            className="w-full h-auto rounded-tl-2xl rounded-tr-2xl @md:rounded-tr-none rounded-bl-none @md:rounded-bl-2xl"
+            className={styles.image}
           />
         </div>
         <div className={styles.infoContainer}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import PRODUCTS from "./products";
 import { ProductCard } from "./ProductCard";
 
-import styles from './page.module.css';
-
 export default function Home() {
   return (
     <main className="mx-auto max-w-[960px] flex flex-wrap">
       {PRODUCTS.map((product) => (
-        <div key={product.id} className={styles.card}>
+        <div key={product.id} className="w-full md:w-1/3 p-2">
           <ProductCard product={product} />
         </div>
       ))}
