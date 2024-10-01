0969af1119cb4417206f24eb2a3faab42b5ac967
isagi-agustin <isagiagustin@outlook.com>
2024-10-01 08:11:16 -0700

    tailwind css container queries

--- src/app/ProductCard.tsx
+++ src/app/ProductCard.tsx
@@ -10,14 +10,15 @@ interface Props {
 
 export const ProductCard = ({ product }: Props) => {
   return (
-    <div className={styles.card}>
-      <div className={styles.cardContainer}>
-        <div className={styles.imageContainer}>
+    <div className="@container">
+      <div className="flex flex-col @md:flex-row">
+        <div className="w-full @md:w-1/4">
           <Image
             src={product.image}
             alt={product.title}
             width={300}
             height={300}
+            className="w-full h-auto rounded-tl-2xl rounded-tr-2xl @md:rounded-tr-none rounded-bl-none @md:rounded-bl-2xl"
           />
         </div>
         <div className={styles.infoContainer}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className="mx-auto max-w-[960px] flex flex-wrap">
       {PRODUCTS.map((product) => (
         <div key={product.id} className={styles.card}>
           <ProductCard product={product} />
